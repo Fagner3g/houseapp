91b05cb6c6738ca7094365d18891712f05ab1903
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-23 12:02:25 -0300

    chore: set up biome fixed

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/", async () => {
-	return { hello: "world" };
-});
+app.get('/', async () => {
+  return { hello: 'world' }
+})
 
-async function start() {
-	try {
-		const address = await app.listen({
-			port: 3333,
-			host: "0.0.0.0",
-		});
-		console.log(`Server listening at ${address}`);
-	} catch (err) {
-		app.log.error(err);
-		process.exit(1);
-	}
-}
-
-start();
+app.listen({ port: 3333, host: '0.0.0.0' }).then(address => {
+  console.log(`Server listening at ${address}`)
+})
