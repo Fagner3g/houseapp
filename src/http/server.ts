4c417865e446c255424401271c10ee6d301c7699
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-23 09:52:12 -0300

    chore: Initialize backend config

---
+++ src/http/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return { hello: "world" };
+});
+
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then((address) => {
+    console.log(`Server listening at ${address}`);
+  });
