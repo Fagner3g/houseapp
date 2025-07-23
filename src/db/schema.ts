e089098e1ba2b2b1b6c9c3a1fa5bcc5100e62e60
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-23 15:15:49 -0300

    feat: Install @paralleldrive/cuid2 for default primary key IDs

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,25 @@
+import { createId } from '@paralleldrive/cuid2'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$default(() => createId()),
   title: text('title').notNull(),
   desiredWeekFrequency: integer('desired_week_frequency').notNull(),
-  createdAt: timestamp('createdAt', { withTimezone: true }).notNull().defaultNow(),
+  createdAt: timestamp('createdAt', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
 })
 
 export const goalCompletions = pgTable('goal_completions', {
-  id: text('id').primaryKey(),
-  goalId: text('goal_id').references(() => goals.id).notNull(),
-  createdAt: timestamp('createdAt', { withTimezone: true }).notNull().defaultNow(),
+  id: text('id')
+    .primaryKey()
+    .$default(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp('createdAt', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
 })
