53b8c400c053b307cd96c19c26d3617c960ec885
Fagner Gomes <fagner.egomes@gmail.com>
2025-07-23 10:27:58 -0300

    Handle server start errors

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,15 +2,21 @@ import fastify from "fastify";
 
 const app = fastify();
 
-app.get("/", () => {
-  return { hello: "world" };
+app.get("/", async () => {
+	return { hello: "world" };
 });
 
-app
-  .listen({
-    port: 3333,
-    host: "0.0.0.0",
-  })
-  .then((address) => {
-    console.log(`Server listening at ${address}`);
-  });
+async function start() {
+	try {
+		const address = await app.listen({
+			port: 3333,
+			host: "0.0.0.0",
+		});
+		console.log(`Server listening at ${address}`);
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+}
+
+start();
