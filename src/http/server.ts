a609eba5322bb159b24d791a56fc5a8574c0bbdb
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-25 13:31:56 -0300

    feat: Create goals route with Zod validation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,31 @@
 import fastify from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import z from 'zod'
 
-const app = fastify()
+import { createGoal } from '../functions/create-goals'
 
-app.get('/', async () => {
-  return { hello: 'world' }
-})
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeekFrequency: z.number(),
+      }),
+    },
+  },
+  async request => {
+    const { desiredWeekFrequency, title } = request.body
+    await createGoal({ desiredWeekFrequency, title })
+  }
+)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(address => {
   console.log(`Server listening at ${address}`)
