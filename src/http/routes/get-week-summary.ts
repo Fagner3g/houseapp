dd97387d61c8c25db54d9679dea984d1505068af
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-25 16:15:05 -0300

    feat: create summary route

--- src/http/routes/get-week-summary.ts
+++ src/http/routes/get-week-summary.ts
@@ -4,8 +4,8 @@ import { getWeekSummary } from '../../functions/get-week-summary'
 
 export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
   app.get('/summary', async () => {
-    const pendingGoals = await getWeekSummary()
+    const { summary } = await getWeekSummary()
 
-    return { pendingGoals }
+    return { summary }
   })
 }
