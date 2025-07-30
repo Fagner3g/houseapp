f85776ce7255f17cbcf6d458fccf87d20c43ff08
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-07-30 17:44:03 -0300

    feat: change name column user

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -17,7 +17,7 @@ export const users = pgTable('users', {
   name: text('name').notNull(),
   email: text('email').notNull(),
   phone: text('phone').notNull(),
-  ddd: text('phone').notNull(),
+  ddd: text('ddd').notNull(),
   avatarUrl: text('avatar_url').notNull(),
   /* Default organization used on signup */
   defaultOrganizationId: text('default_organization_id')
