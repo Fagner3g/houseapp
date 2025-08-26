bbbcccdf233c352ed2a3b8989b14d5f3a5880744
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-08-26 09:19:30 -0300

    feat: update page 404

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -5,7 +5,7 @@ export function NotFoundPage() {
     <div className="flex flex-col items-center justify-center gap-3">
       <h1 className="text-9xl text-red-500">404</h1>
       <p>Página não encontrada, volte para a página de login</p>
-      <Link to="/sign-in">Login</Link>
+      <Link to="/sign-in">Voltar para o login</Link>
     </div>
   )
 }
