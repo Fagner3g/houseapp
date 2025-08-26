9b25649eefb9beb5cba839bf2c3e451a9d1476e2
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-08-26 18:48:41 -0300

    feat: add health check endpoints and simplify CI/CD workflow

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -4,7 +4,7 @@ export function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-3">
       <h1 className="text-9xl text-red-500">404</h1>
-      <p>Página não encontrada, volte para a página de login</p>
+      <p>Página não encontrada, volte para a página de login ou entre em contato com o suporte</p>
       <Link to="/sign-in">Voltar para o login</Link>
     </div>
   )
