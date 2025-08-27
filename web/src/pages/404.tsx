5352191dc6b025707c4608ed62bd52fa58b891c0
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-08-26 21:07:45 -0300

    feat: workflow completo para deploy da stack no swarm

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -5,7 +5,7 @@ export function NotFoundPage() {
     <div className="flex flex-col items-center justify-center gap-3">
       <h1 className="text-9xl text-red-500">404</h1>
       <p>Página não encontrada, volte para a página de login ou entre em contato com o suporte</p>
-      <Link to="/sign-in">Voltar para o logi</Link>
+      <Link to="/sign-in">Voltar para o login</Link>
     </div>
   )
 }
