a1bb18fee17e566186539eb64fe85e1ec5c53f1e
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-09-16 19:52:11 -0300

    test: força execução do CI/CD para testar workflow simplificado

--- web/src/pages/_auth/sign-in.tsx
+++ web/src/pages/_auth/sign-in.tsx
@@ -69,7 +69,7 @@ function Index() {
           Cadastrar
         </Button>
       </div>
-      <h4>v1.0.1 - Deploy Test - Final</h4>
+      <h4>v1.0.1 - Deploy Test - Simplificado</h4>
     </div>
   )
 }
