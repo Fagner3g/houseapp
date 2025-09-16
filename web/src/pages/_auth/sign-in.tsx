06d26a884a00fce01c1abc0782d6a7616a3057de
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-09-16 20:03:46 -0300

    test: atualiza para v1.0.3 - Workflow Corrigido
    - Testa o workflow CI/CD corrigido
    - Verifica se o deploy automático funciona após as correções

--- web/src/pages/_auth/sign-in.tsx
+++ web/src/pages/_auth/sign-in.tsx
@@ -69,7 +69,7 @@ function Index() {
           Cadastrar
         </Button>
       </div>
-      <h4>v1.0.2 - Teste de Deploy Automático</h4>
+      <h4>v1.0.3 - Workflow Corrigido</h4>
     </div>
   )
 }
