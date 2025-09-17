10f25eeab4998ed5dcc637d133d6d40296f427c7
Fagner Gomes <fagner.gomes@davinti.com.br>
2025-09-16 21:12:25 -0300

    test: atualiza para v1.0.2 - Teste Deploy Final
    - Testa se o deploy automático está funcionando corretamente
    - Verifica se as alterações são refletidas na web
    - Monitora o processo completo de deploy

--- web/src/pages/_auth/sign-in.tsx
+++ web/src/pages/_auth/sign-in.tsx
@@ -69,7 +69,7 @@ function Index() {
           Cadastrar
         </Button>
       </div>
-      <h4>v1.0.1 - Deploy Automático Completo</h4>
+      <h4>v1.0.2 - Teste Deploy Final</h4>
     </div>
   )
 }
